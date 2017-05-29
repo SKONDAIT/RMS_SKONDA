401a6e8031ec8faa385b222e33db22b60248fb29
skondaitservices <info@skonda.in>
2017-05-29 18:58:43 +0530

    added interface
    OnKeyListener

--- app/src/main/java/in/skonda/rms_skonda/Login.java
+++ app/src/main/java/in/skonda/rms_skonda/Login.java
@@ -2,12 +2,21 @@ package in.skonda.rms_skonda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+
 // login activity - sudarsan.konda
-public class Login extends AppCompatActivity {
+// added interface OnKeyListener
+public class Login extends AppCompatActivity implements View.OnKeyListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        return false;
     }
 }
