f7e854a0d4e73c478994e8e757be817ed42fb7b9
skondaitservices <info@skonda.in>
2017-06-03 09:07:25 +0530

    View.OnClickListener on dashboard

--- app/src/main/java/in/skonda/rms_skonda/Dashboard.java
+++ app/src/main/java/in/skonda/rms_skonda/Dashboard.java
@@ -2,12 +2,56 @@ package in.skonda.rms_skonda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView enroll;
+    ImageView list;
+    ImageView student;
+    ImageView fee;
+    ImageView sms;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        enroll = (ImageView) findViewById(R.id.enroll);
+        list = (ImageView) findViewById(R.id.list);
+        student = (ImageView) findViewById(R.id.student);
+        fee = (ImageView) findViewById(R.id.fee);
+        sms = (ImageView) findViewById(R.id.sms);
+
+        enroll.setOnClickListener(this);
+        list.setOnClickListener(this);
+        student.setOnClickListener(this);
+        fee.setOnClickListener(this);
+        sms.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.enroll:
+                Toast.makeText(this, "enroll clickedx", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.list:
+                Toast.makeText(this, "list clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.student:
+                Toast.makeText(this, "student clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fee:
+                Toast.makeText(this, "fee clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sms:
+                Toast.makeText(this, "sms clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
