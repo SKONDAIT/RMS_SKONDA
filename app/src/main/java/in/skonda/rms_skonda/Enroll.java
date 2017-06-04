b5b512cdd647a5079eecaea237fdd445ce130800
anuja-k <anuja.kurakula@gmail.com>
2017-06-04 16:19:47 +0530

    updated enroll activity

--- app/src/main/java/in/skonda/rms_skonda/Enroll.java
+++ app/src/main/java/in/skonda/rms_skonda/Enroll.java
@@ -1,18 +1,37 @@
 package in.skonda.rms_skonda;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class Enroll extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
+public class Enroll extends AppCompatActivity implements View.OnClickListener {
+    private EditText doe;
+    private EditText doj;
+    private EditText dob;
+
+    private DatePickerDialog doeDatePickerDialog;
+    private DatePickerDialog dojDatePickerDialog;
+    private DatePickerDialog dobDatePickerDialog;
+
+    private SimpleDateFormat dateFormatter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll);
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        findViewsById();
+        setDateTimeField();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,4 +46,66 @@ public class Enroll extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+
+    private void findViewsById() {
+        doe = (EditText) findViewById(R.id.input_doe);
+        doe.setInputType(InputType.TYPE_NULL);
+
+        doj = (EditText) findViewById(R.id.input_doj);
+        doj.setInputType(InputType.TYPE_NULL);
+
+        dob = (EditText) findViewById(R.id.input_dob);
+        dob.setInputType(InputType.TYPE_NULL);
+    }
+
+    private void setDateTimeField() {
+        doe.setOnClickListener(this);
+        doj.setOnClickListener(this);
+        dob.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        doeDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                doe.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        dojDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                doj.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        dobDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                dob.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == doe) {
+            doeDatePickerDialog.show();
+        } else if(view == doj) {
+            dojDatePickerDialog.show();
+        } else if(view == dob){
+            dobDatePickerDialog.show();
+        }
+
+    }
 }
