64f8b5fff3784aaba313d0e1856cc4a9c8ce51b9
anuja-k <anuja.kurakula@gmail.com>
2017-06-06 10:06:00 +0530

    change to sms.java

--- app/src/main/java/in/skonda/rms_skonda/sms.java
+++ app/src/main/java/in/skonda/rms_skonda/sms.java
@@ -24,6 +24,7 @@ public class sms extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
