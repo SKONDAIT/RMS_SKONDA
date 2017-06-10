41b1375ea1b7e58cdc31eff54835555f11097811
skondaitservices <info@skonda.in>
2017-06-10 11:25:06 +0530

    added fcm and some screens

---
+++ app/src/main/java/in/skonda/rms_skonda/FeePay.java
@@ -0,0 +1,13 @@
+package in.skonda.rms_skonda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeePay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fee_pay);
+    }
+}
